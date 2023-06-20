40aa086aa6bfe945b3fc1a3947f8f8af85472478
acet09 <ysjungman@gmail.com>
2023-06-20 11:11:25 +0900

    test code

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Card from '../UI/Card';
 import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
+  const [inputValue, setInputValue] = useState('');
+
+  const inputChangeHandler = event => {
+    setInputValue(event.target.value);
+  }
+
   const submitHandler = event => {
     event.preventDefault();
-    // ...
+    console.log(inputValue);
   };
 
   return (
@@ -15,14 +21,14 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" />
+            <input type="text" id="title" value={inputValue} onChange={inputChangeHandler} />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
             <input type="number" id="amount" />
           </div>
           <div className="ingredient-form__actions">
-            <button type="submit">Add Ingredient</button>
+            <button onClick={submitHandler} type="submit">Add Ingredient</button>
           </div>
         </form>
       </Card>
