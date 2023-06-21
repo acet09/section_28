c37d6e482e58669f4b07c0594c3a4ff81b2e226b
Cactus <ysjungman@gmail.com>
2023-06-22 08:20:21 +0900

    470_useReducer

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,23 +1,37 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useReducer, useState, useEffect, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
 import ErrorModal from '../UI/ErrorModal';
 import Search from './Search';
 
+const ingredientReducer = (currentIngredients, action) => {
+  switch (action.type) {
+    case 'SET':
+      return action.ingredients;
+    case 'ADD':
+      return [...currentIngredients, action.ingredient];
+    case 'DELETE':
+      return currentIngredients.filter(ing => ing.id !== action.id)
+    default:
+      throw new Error('Should not get there!');
+  }
+}
+
 const Ingredients = () => {
-  const [userIngredients, setUserIngredients] = useState([]);
+  const [userIngredients, dispatch] = useReducer(ingredientReducer, []);
+  // const [userIngredients, setUserIngredients] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState();
-
-  const filteredIngredientsHandler = useCallback(filteredIngredients => {
-    setUserIngredients(filteredIngredients);
-  }, []);
 
   useEffect(() => {
     console.log('RENDERING INGREDIENTS', userIngredients);
   }, [userIngredients]);
 
+  const filteredIngredientsHandler = useCallback(filteredIngredients => {
+    // setUserIngredients(filteredIngredients);
+    dispatch({ type: 'SET', ingredients: filteredIngredients });
+  }, []);
 
   const addIngredientHandler = ingredient => {
     setIsLoading(true);
@@ -25,29 +39,35 @@ const Ingredients = () => {
       method: 'POST',
       body: JSON.stringify(ingredient),
       headers: { 'Content-Type': 'application/json' }
-    }).then(response => {
-      setIsLoading(false);
-      return response.json();
-    }).then(responseData => {
-      setUserIngredients(prevIngredients => [
-        ...prevIngredients,
-        { id: responseData.name, ...ingredient }
-      ]);
     })
+      .then(response => {
+        setIsLoading(false);
+        return response.json();
+      })
+      .then(responseData => {
+        // setUserIngredients(prevIngredients => [
+        //   ...prevIngredients,
+        //   { id: responseData.name, ...ingredient }
+        // ]);
+        dispatch({ type: 'ADD', ingredient: { id: responseData.name, ...ingredient } })
+      })
   };
 
   const removeIngredientHandler = ingredientId => {
     setIsLoading(true);
     fetch(
-      `https://react-hook-e01fd-default-rtdb.firebaseio.com/ingredients/${ingredientId}.jon`, {
+      `https://react-hook-e01fd-default-rtdb.firebaseio.com/ingredients/${ingredientId}.json`, {
       method: 'DELETE',
-    }).then(response => {
-      setIsLoading(false);
-      setUserIngredients(prevIngredients => prevIngredients.filter((ingredient) => ingredient.id !== ingredientId));
-    }).catch(error => {
-      setError(error.message, '에러가 발생했습니다');
-      setIsLoading(false);
-    });
+    })
+      .then(response => {
+        setIsLoading(false);
+        // setUserIngredients(prevIngredients => prevIngredients.filter((ingredient) => ingredient.id !== ingredientId));
+        dispatch({ type: 'DELETE', id: ingredientId })
+      })
+      .catch(error => {
+        setError(error.message, '에러가 발생했습니다');
+        setIsLoading(false);
+      });
   };
 
   const clearError = () => {
